9de21f567ab825f6784c214ec014d00778896744
Alexander Mills <alexander_mills2@cable.comcast.com>
2016-03-09 21:17:25 -0800

    FC

---
+++ index.js
@@ -0,0 +1,92 @@
+/**
+ * Created by amills001c on 3/9/16.
+ */
+
+
+const async = require('async');
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+const os = require('os');
+
+const args = process.argv.slice(2);
+const $path = args[0] || process.cwd();
+
+
+const gitPaths = [];
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function recurse(dir) {
+
+    var stat;
+
+    fs.readdirSync(dir).forEach(function (item) {
+
+        item = path.resolve(path.normalize(dir + '/' + item));
+
+        if (endsWith(item, '.git')) {
+            gitPaths.push(item);
+        }
+        else {
+            stat = fs.statSync(item);
+            if (stat.isDirectory()) {
+                recurse(item);
+            }
+        }
+
+    });
+
+}
+
+recurse($path);
+
+console.log(gitPaths);
+
+async.each(gitPaths, function (item, cb) {
+
+    const orig = String(path.normalize(item));
+    var arr = item.split(path.sep);
+    arr.pop();
+    item = arr.join(path.sep);
+
+    var command;
+    if (os.platform() === 'win32') {
+        command = 'cd ' + path.normalize(item);
+    }
+    else {
+        command = 'cd ' + path.normalize(item) + ' && git status --s';
+    }
+
+    cp.exec(command, {}, function (err, data) {
+        if (err) {
+            cb(err);
+        }
+        else{
+            var result = String(data).split('\n');
+            if(result.length < 1){
+                cb(null);
+            }
+
+        }
+    });
+
+
+}, function complete(err, results) {
+
+    if (err) {
+        console.log(err);
+    }
+    else {
+        results.filter(function(item){
+            return item && String(item).length > 0;
+        }).forEach(function(item){
+            console.log(item);
+        });
+    }
+
+});
+
+
